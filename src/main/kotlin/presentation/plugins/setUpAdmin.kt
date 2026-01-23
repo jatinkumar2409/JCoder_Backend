2c01ac98bb04f172ee3295ab83467d893f3b4f4d
jatinkumar2409 <jnkr2409@gmail.com>
2026-01-23 23:06:54 +0530

    set up admin

--- src/main/kotlin/presentation/plugins/setUpAdmin.kt
+++ src/main/kotlin/presentation/plugins/setUpAdmin.kt
@@ -1,5 +1,6 @@
 package com.example.presentation.plugins
 
+import com.example.data.helpers.getEnv
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
@@ -7,7 +8,9 @@ import io.ktor.server.application.Application
 
 
 fun Application.setUpAdmin(){
-    val service = this::class.java.classLoader.getResourceAsStream("admin_sdk.json")
+//    val service = this::class.java.classLoader.getResourceAsStream("admin_sdk.json")
+    val json = getEnv("FIREBASE_ADMIN")
+    val service = json.byteInputStream()
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(service))
         .build()
